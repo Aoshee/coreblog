85cadf6e34a4b41e536a5e9842e3649ba6e49a49
Aoshee <anhtai@fvnup.com>
2016-08-03 10:31:03 +0700

    Update settings.py

--- sec_blog/settings.py
+++ sec_blog/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'sec_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates/")],
+        'DIRS': [os.path.join(BASE_DIR, "core/")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
